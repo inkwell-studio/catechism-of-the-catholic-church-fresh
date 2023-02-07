da9ae6551463889951c3ee06a63eb698f9be3a11
InkWell Studio <joepro@tuta.io>
2023-02-06 21:24:50 -0600

    feat(web app): add info to home page

--- web-app/routes/index.tsx
+++ web-app/routes/index.tsx
@@ -11,15 +11,18 @@ export default function Home() {
                     <h1 class='font-serif font-bold text-4xl md:text-5xl lg:text-6xl text-center'>
                         Catechism of&nbsp;the Catholic&nbsp;Church
                     </h1>
-                    <h2 class='italic text-xl md:text-2xl lg:text-3xl my-4'>Proof of Concept</h2>
+                    <h2 class='italic text-xl md:text-2xl lg:text-3xl my-6'>Proof of Concept</h2>
                 </div>
 
-                <div class='max-w-xl md:max-w-2xl px-8'>
-                    <div class='bg-red-900 w-full h-px mb-4'>&nbsp;</div>
+                <div class='max-w-xl md:max-w-2xl px-8 mb-16'>
+                    <div class='bg-red-900 w-full h-px mb-8'>&nbsp;</div>
 
                     <Title>What This Is</Title>
                     <Paragraph>
-                        A step toward a better online version of the{' '}
+                        A proof-of-concept for how the <span class="italic">Catechism of the Catholic&nbsp;Church</span> can be made more user-friendly for the general online public.
+                    </Paragraph>
+                    <Paragraph>
+                        It is a step toward a better online version of the{' '}
                         <span class='italic'>Catechism of the Catholic Church</span>.
                     </Paragraph>
 
@@ -28,11 +31,9 @@ export default function Home() {
                         To provide a more fitting digital home for one of the Church's great treasures.
                     </Paragraph>
 
-                    <Title>Next Steps</Title>
+                    <Title>What Comes Next</Title>
                     <Paragraph>
-                        Licensing permission needs to be obtained from the copyright holders (the United&nbsp;States
-                        Conference of Catholic&nbsp;Bishops holds the copyright for the English translation of the{' '}
-                        <span className='italic'>Catechism</span>).
+                        Licensing permission needs to be obtained before the actual text of the <span class="italic">Catechism</span> can be used here (the United&nbsp;States Conference of Catholic&nbsp;Bishops holds the copyright for the English translation).
                     </Paragraph>
                     <Paragraph>
                         Also, much development work remains.
@@ -40,9 +41,12 @@ export default function Home() {
 
                     <Title>Contact</Title>
                     <Paragraph>
-                        If you would like more information, please email me, the sole developer, at{' '}
-                        <a class='italic' href='mailto:joecode@tuta.io'>joecode@tuta.io</a>.
+                        If you would like more information, you may view the source code or email me, the sole developer:
                     </Paragraph>
+                    <div className="my-2 flex justify-center">
+                        <ButtonLink href='mailto:joecode@tuta.io' text='source code'></ButtonLink>
+                        <ButtonLink href='mailto:joecode@tuta.io' text='joecode@tuta.io'></ButtonLink>
+                    </div>
                 </div>
             </body>
         </>
@@ -59,4 +63,8 @@ function Content({ children }) {
 
 function Paragraph({ children }) {
     return <p class='px-4 mb-2'>{children}</p>;
+}
+
+function ButtonLink({ href, text }) {
+    return <a class="inline-block font-sans border border-2 rounded bg-tan-100 hover:bg-tan-50 transition-bg p-3 mt-2 mr-3" href={href}>{text}</a>
 }
