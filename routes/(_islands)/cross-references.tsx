c6535383b8f267ad3ce717566854cd251ee4c536
InkWell Studio <joepro@tuta.io>
2023-09-22 18:46:37 -0500

    improvements

--- routes/(_islands)/cross-references.tsx
+++ routes/(_islands)/cross-references.tsx
@@ -6,17 +6,22 @@ import { Language, NumberOrNumberRange, Paragraph } from '../../catechism/source
 import { getParagraphNumbers } from '../../catechism/source/utils/content.ts';
 import { clearCrossReferenceSelection, state } from '../../web/state.ts';
 
+// TODO: Load data
+
 export default function CrossReferences(props: { paragraphs: Array<Paragraph>; language: Language }): JSX.Element {
     const refs = state.value.selectedCrossReferences;
     const latestSelectedReference = refs.at(-1);
 
     if (latestSelectedReference) {
         return (
-            <div className='flex flex-col gap-2'>
-                <div>
-                    {Trail(refs.slice(0, 1))}
+            <div className='fixed top-8 right-4 flex flex-col items-start gap-2 p-12 rounded-lg bg-white'>
+                <button onClick={clearCrossReferenceSelection} class="self-end">Close</button>
+                <div class="border-b">
+                    {Trail(state.value.selectedCrossReferences)}
                 </div>
-                {Content(latestSelectedReference, props.paragraphs, props.language)}
+                <div>
+                    {Content(latestSelectedReference, props.paragraphs, props.language)}
+                </div>
             </div>
         );
     } else {
@@ -28,30 +33,21 @@ function Content(
     selectedReference: NumberOrNumberRange,
     paragraphs: Array<Paragraph>,
     language: Language,
-): JSX.Element {
+): Array<JSX.Element> {
     const paragraphNumbers = getParagraphNumbers([selectedReference]);
     const crossReferences = paragraphs.filter((p) => paragraphNumbers.includes(p.paragraphNumber));
 
-    return (
-        <div class='fixed top-8 right-4 p-12 rounded-lg bg-white'>
-            <div class='space-y-4'>
-                <div>{selectedReference}</div>
-                <button onClick={clearCrossReferenceSelection}>Close</button>
-            </div>
-            <div className='border border-black border-2'>
-                {crossReferences.map((ref) => <ContentBase key={ref} content={ref} language={language}></ContentBase>)}
-            </div>
-        </div>
-    );
+    return crossReferences.map((ref) => <ContentBase key={ref} content={ref} language={language}></ContentBase>)
 }
 
-function Trail(references: Array<NumberOrNumberRange>): Array<JSX.Element> {
-    return references.map((ref, index) => {
-        const finalReference = index === references.length - 1;
+function Trail(refs: Array<NumberOrNumberRange>): Array<JSX.Element> {
+    return refs.map((ref, index) => {
+        const finalReference = index === refs.length - 1;
+        // TODO: Implement proper `onClick` functionality
         return (
             <>
-                <span key={ref}>ref{finalReference ? ', ' : ''}</span>
-                {finalReference ? <>' '</> : <></>};
+                <button key={ref}>{ref}{finalReference ? '' : ','}</button>
+                {finalReference ? <></> : ' '}
             </>
         );
     });
