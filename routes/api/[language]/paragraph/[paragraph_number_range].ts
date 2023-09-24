a94418d40a4f65f5b4b7c8d041f9c698debe8558
InkWell Studio <joepro@tuta.io>
2023-09-24 15:33:39 -0500

    now loading paragraphs via the API

--- routes/api/[language]/paragraph/[paragraph_number_range].ts
+++ routes/api/[language]/paragraph/[paragraph_number_range].ts
@@ -8,11 +8,10 @@ export const handler: Handlers<Paragraph | null> = {
     async GET(_request, context) {
         const languageInfo = getLanguageInfo(context.params.language);
         if (languageInfo.language && languageInfo.supported) {
+            // TODO: Use the simpler `paragraph-number_to_content-en.json` content map instead, and remove all logic and data of this ~`paragraph-cross-reference-to-content` content map
             const contentMap = await getParagraphCrossReferenceContentMap(languageInfo.language);
-            const selection = context.params['paragraph-number-range'] as NumberOrNumberRange;
-            // TODO: Finish
-            // return new Response(JSON.stringify(contentMap[selection]));
-            return new Response(JSON.stringify({ test: '123abc' }));
+            const selection = context.params['paragraph_number_range'] as NumberOrNumberRange;
+            return new Response(JSON.stringify(contentMap[selection]));
         } else {
             return new Response(null);
         }
