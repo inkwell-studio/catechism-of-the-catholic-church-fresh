258805433b3e3008455c954e852faccdd89eb3f5
InkWell Studio <joepro@tuta.io>
2023-09-23 21:21:16 -0500

    another version

--- catechism/artifact-builders/paragraph-cross-reference-to-content-map.ts
+++ catechism/artifact-builders/paragraph-cross-reference-to-content-map.ts
@@ -3,20 +3,33 @@ import { ParagraphCrossReferenceContentMap } from '../source/types/types.ts';
 import { getParagraphNumbers, getTextWrappers } from '../source/utils/content.ts';
 
 export function build(contentMap: ParagraphNumberContentMap): ParagraphCrossReferenceContentMap {
+    const crossReferenceContentMap: ParagraphCrossReferenceContentMap = {};
+
+    Object.values(contentMap)
+        .flatMap((paragraph) => getTextWrappers(paragraph))
+        .filter((textWrapper) => textWrapper.paragraphReferences.length > 0)
+        .flatMap((textWrapper) => textWrapper.paragraphReferences)
+        .forEach((crossReference) => {
+            const paragraphs = getParagraphNumbers([crossReference])
+                .map((paragraphNumber) => contentMap[paragraphNumber]);
+
+            crossReferenceContentMap[crossReference] = paragraphs;
+        });
+
+    return crossReferenceContentMap;
+}
+
+/*/
+// TODO: Use this if it works and is more readable than the implementation above
+export function build(contentMap: ParagraphNumberContentMap): ParagraphCrossReferenceContentMap {
     return Object.values(contentMap)
         .flatMap((paragraph) => getTextWrappers(paragraph))
         .filter((textWrapper) => textWrapper.paragraphReferences.length > 0)
         .flatMap((textWrapper) => textWrapper.paragraphReferences)
-        /*/
-            .map((crossReference) => {
-                const paragraphs = getParagraphNumbers([crossReference])
-                .map((paragraphNumber) => contentMap[paragraphNumber]);
-
-                return { [crossReference]:  paragraphs }
-                /*/
         .map((crossReference) => ({
             [crossReference]: getParagraphNumbers([crossReference])
                 .map((paragraphNumber) => contentMap[paragraphNumber]),
         }))
         .reduce((previous, current) => ({ ...previous, ...current }), {});
 }
+/*/
