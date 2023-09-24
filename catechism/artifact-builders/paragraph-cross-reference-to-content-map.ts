1b6f2df488d8b973bf76392acff302267e9c2efc
InkWell Studio <joepro@tuta.io>
2023-09-23 21:18:47 -0500

    draft

--- catechism/artifact-builders/paragraph-cross-reference-to-content-map.ts
+++ catechism/artifact-builders/paragraph-cross-reference-to-content-map.ts
@@ -3,18 +3,20 @@ import { ParagraphCrossReferenceContentMap } from '../source/types/types.ts';
 import { getParagraphNumbers, getTextWrappers } from '../source/utils/content.ts';
 
 export function build(contentMap: ParagraphNumberContentMap): ParagraphCrossReferenceContentMap {
-    const crossReferenceContentMap: ParagraphCrossReferenceContentMap = {};
-
-    Object.values(contentMap)
+    return Object.values(contentMap)
         .flatMap((paragraph) => getTextWrappers(paragraph))
         .filter((textWrapper) => textWrapper.paragraphReferences.length > 0)
         .flatMap((textWrapper) => textWrapper.paragraphReferences)
-        .forEach((crossReference) => {
-            const paragraphs = getParagraphNumbers([crossReference])
+        /*/
+            .map((crossReference) => {
+                const paragraphs = getParagraphNumbers([crossReference])
                 .map((paragraphNumber) => contentMap[paragraphNumber]);
 
-            crossReferenceContentMap[crossReference] = paragraphs;
-        });
-
-    return crossReferenceContentMap;
+                return { [crossReference]:  paragraphs }
+                /*/
+        .map((crossReference) => ({
+            [crossReference]: getParagraphNumbers([crossReference])
+                .map((paragraphNumber) => contentMap[paragraphNumber]),
+        }))
+        .reduce((previous, current) => ({ ...previous, ...current }), {});
 }
