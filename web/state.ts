d7ed6f0e7a6c8965893422bf713d84f6fed468bf
InkWell Studio <joepro@tuta.io>
2023-10-04 16:30:10 -0500

    refactor state

--- web/state.ts
+++ web/state.ts
@@ -1,4 +1,4 @@
-import { computed, signal } from '@preact/signals';
+import { batch, computed, signal, Signal} from '@preact/signals';
 
 import * as server from './server.ts';
 import { Language, NumberOrNumberRange, Paragraph } from '../catechism/source/types/types.ts';
@@ -12,23 +12,23 @@ import { Language, NumberOrNumberRange, Paragraph } from '../catechism/source/ty
 
 //#region state
 type State = {
-    language: Language;
-    showChangelog: boolean;
+    language: Signal<Language>;
+    showChangelog: Signal<boolean>;
     crossReference: {
-        selectedContent: Array<Paragraph>;
+        selectedContent: Signal<Array<Paragraph>>;
         // A stack of the cross-references selected by the user. The most recent selection is at the end of the array.
-        selectionHistory: Array<NumberOrNumberRange>;
+        selectionHistory: Signal<Array<NumberOrNumberRange>>;
     };
 };
 
-const state = signal<State>({
-    language: Language.ENGLISH,
-    showChangelog: false,
+const state: State = {
+    language: signal(Language.ENGLISH),
+    showChangelog: signal(false),
     crossReference: {
-        selectedContent: [],
-        selectionHistory: [],
+        selectedContent: signal([]),
+        selectionHistory: signal([]),
     },
-});
+};
 
 const CROSS_REFERENCE_HISTORY_LIMIT = 7;
 //#endregion
@@ -52,72 +52,60 @@ export const Actions = {
 
 //#region changelog
 function openChangelog(): void {
-    state.value = {
-        ...state.value,
-        showChangelog: true,
-    };
+    state.showChangelog.value = true;
 }
 
 function closeChangelog(): void {
-    state.value = {
-        ...state.value,
-        showChangelog: false,
-    };
+    state.showChangelog.value = false;
 }
 //#endregion
 
 //#region language
 function updateLanguage(language: Language): void {
-    state.value = { ...state.value, language };
+    state.language.value = language;
 }
 //#endregion
 
 //#region cross-references
-async function selectCrossReference(reference: NumberOrNumberRange): Promise<void> {
-    if (state.value.crossReference.selectionHistory.at(-1) !== reference) {
-        const selectionHistory = state.value.crossReference.selectionHistory.concat(reference);
+function selectCrossReference(reference: NumberOrNumberRange): void {
+    if (state.crossReference.selectionHistory.value.at(-1) !== reference) {
+        const selectionHistory = state.crossReference.selectionHistory.value.concat(reference);
         if (selectionHistory.length > CROSS_REFERENCE_HISTORY_LIMIT) {
             selectionHistory.shift();
         }
-        const s = updateCrossReferenceSelectionHistory(state.value, selectionHistory);
-        state.value = await loadCrossReferenceSelectedContent(s, reference);
+        batch(() => {
+            updateCrossReferenceSelectionHistory(selectionHistory);
+            loadCrossReferenceSelectedContent(reference);
+        });
     }
 }
 
-async function selectCrossReferenceFromHistory(index: number): Promise<void> {
-    const selectionHistory = state.value.crossReference.selectionHistory.slice(0, index + 1);
-    const s = updateCrossReferenceSelectionHistory(state.value, selectionHistory);
-    state.value = await loadCrossReferenceSelectedContent(s, selectionHistory.at(-1) as NumberOrNumberRange);
+function selectCrossReferenceFromHistory(index: number): void {
+    const selectionHistory = state.crossReference.selectionHistory.value.slice(0, index + 1);
+    batch(() => {
+        updateCrossReferenceSelectionHistory(selectionHistory);
+        loadCrossReferenceSelectedContent(selectionHistory.at(-1) as NumberOrNumberRange);
+    });
 }
 
-async function loadCrossReferenceSelectedContent(s: State, paragraphNumbers: NumberOrNumberRange): Promise<State> {
-    const paragraphs = await server.getParagraphs(paragraphNumbers, s.language);
-    return updateCrossReferenceSelectedContent(s, paragraphs);
+async function loadCrossReferenceSelectedContent(paragraphNumbers: NumberOrNumberRange): Promise<void> {
+    const paragraphs = await server.getParagraphs(paragraphNumbers, state.language.value);
+    updateCrossReferenceSelectedContent(paragraphs);
 }
 
 function clearCrossReferenceSelection(): void {
-    const s = updateCrossReferenceSelectionHistory(state.value, []);
-    state.value = updateCrossReferenceSelectedContent(s, []);
+    batch(() => {
+        updateCrossReferenceSelectionHistory([]);
+        updateCrossReferenceSelectedContent([]);
+    });
 }
 
-function updateCrossReferenceSelectedContent(s: State, selectedContent: Array<Paragraph>): State {
-    return {
-        ...s,
-        crossReference: {
-            ...s.crossReference,
-            selectedContent,
-        },
-    };
+function updateCrossReferenceSelectedContent(selectedContent: Array<Paragraph>): void {
+    state.crossReference.selectedContent.value = selectedContent;
 }
 
-function updateCrossReferenceSelectionHistory(s: State, selectionHistory: Array<NumberOrNumberRange>): State {
-    return {
-        ...s,
-        crossReference: {
-            ...s.crossReference,
-            selectionHistory,
-        },
-    };
+function updateCrossReferenceSelectionHistory(selectionHistory: Array<NumberOrNumberRange>): void {
+    state.crossReference.selectionHistory.value = selectionHistory;
 }
 //#endregion
 //#endregion
@@ -125,13 +113,13 @@ function updateCrossReferenceSelectionHistory(s: State, selectionHistory: Array<
 //#region Selectors
 // Selectors are named as: [noun].[verb]
 export const Selectors = {
-    language: computed(() => state.value.language),
+    language: computed(() => state.language.value),
     changelog: {
-        show: computed(() => state.value.showChangelog),
+        show: computed(() => state.showChangelog.value),
     },
     crossReference: {
-        selectedContent: computed(() => state.value.crossReference.selectedContent),
-        selectionHistory: computed(() => state.value.crossReference.selectionHistory),
+        selectedContent: computed(() => state.crossReference.selectedContent.value),
+        selectionHistory: computed(() => state.crossReference.selectionHistory.value),
     },
 } as const;
 //#endregion
