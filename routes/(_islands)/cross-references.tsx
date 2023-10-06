cf13b4cc6b27a96718493ee5cdd5e4e1b3b71b02
InkWell Studio <joepro@tuta.io>
2023-10-06 16:26:52 -0500

    slight UI improvements

--- routes/(_islands)/cross-references.tsx
+++ routes/(_islands)/cross-references.tsx
@@ -13,19 +13,18 @@ export default function CrossReferences(): JSX.Element {
 
     if (content.length > 0) {
         return (
-            <div class='fixed bottom-0 inset-x-0 bg-white pb-8 border-t border-black'>
+            <div class='fixed bottom-0 inset-x-0 bg-white pb-8 px-4 sm:px-12 border-t border-black'>
                 <div class='absolute top-4 right-4 flex gap-2 text-lg font-mono'>
                     <button onClick={() => Actions.crossReference.navigateTo()}>Open</button>
                     {/* TODO: Replace "X" with an icon */}
                     <button onClick={() => Actions.crossReference.clearSelection()}>X</button>
                 </div>
                 <div class='w-full md:max-w-2xl lg:max-w-3xl mx-auto mt-2'>
-                    {showTrail ?
+                    {showTrail && (
                         <div class='flex gap-1 py-2 border-b'>
                             {Trail(selectionHistory)}
                         </div>
-                        : <></>
-                    }
+                    )}
                     <div class={showTrail ? 'mt-2' : 'mt-4'}>
                         {Content(content, language)}
                     </div>
@@ -49,11 +48,15 @@ function Content(
 function Trail(refs: Array<NumberOrNumberRange>): Array<JSX.Element> {
     return refs.map((ref, index) => {
         const finalReference = index === refs.length - 1;
-        return finalReference ? <div key={ref}>{ref}</div> : (
-            <>
-                <button key={ref} onClick={() => Actions.crossReference.selectFromHistory(index)}>{ref},</button>
-                {' '}
-            </>
-        );
+        if (finalReference) {
+            return <div key={ref}>{ref}</div>;
+        } else {
+            return (
+                <>
+                    <button key={ref} onClick={() => Actions.crossReference.selectFromHistory(index)}>{ref},</button>
+                    {' '}
+                </>
+            );
+        }
     });
 }
